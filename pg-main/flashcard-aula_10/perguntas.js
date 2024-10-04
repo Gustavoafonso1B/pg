e97ab72bfd8a4ada73921a5f93baabc776961de4
Gustavoafonso1B <133045808+Gustavoafonso1B@users.noreply.github.com>
2024-10-04 10:23:48 -0300

    Add files via upload

---
+++ pg-main/flashcard-aula_10/perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'voleibol',
+    'Quantos componentes formam uma equipe de voleibol?',
+    'Cada equipe é composta por 6 jogadores(as) em quadra, os(as) quais ocupam metade desse espaço (9m X 9m).'
+)
+
+criaCartao(
+    'boxe',
+    'Qual o principal foco do boxe?',
+    'O objetivo é nocautear o oponente ou acumular pontos durante os rounds.'
+)
+
+criaCartao(
+    'video game',
+    'Qual é o objetivo do vídeo game?',
+    'Quando foram lançados, o foco principal dos videogames era proporcionar entretenimento.'
+)
+
+criaCartao(
+    'voleibol',
+    'Como o voleibol é praticado?',
+    'Após o saque, cada time só poderá tocar a bola três vezes, sendo proibido que um jogador toque a bola duas vezes seguidas.'
+)
+
+criaCartao(
+    'boxe',
+    'Qual é a regra do boxe?',
+    'O boxeador só pode atingir o oponente com socos, com a luva fechada, acima da linha da cintura, na frente e nas laterais.'
+)
+
+criaCartao(
+    'video game',
+    'O que os games ajudam?',
+    'No entanto, os benefícios dos videogames incluem o desenvolvimento de habilidades tais como: atenção, criatividade, memória, idiomas e trabalho em equipe. '
+)
+
+criaCartao(
+    'voleibol',
+    'Quais são as regras para determinar o vencedor?',
+    'Cada set é terminado em 25 pontos, tendo 2 pontos de vantagem sobre a equipe adversária. Caso não tenha, o set prossegue até que uma equipe conquiste vantagem.'
+)
+
+criaCartao(
+    'boxe',
+    'O que é mais importante no boxe?',
+    'A capacidade de se defender efetivamente contra os golpes do oponente é crucial no boxe. '
+)
+
+criaCartao(
+    'video game',
+    'Qual é a origem do vídeo game?',
+    'A ideia surgiu com Ralph Baer em 1966, com o intuito de criar algo que pudesse interagir com a TV.'
+)
+
+criaCartao(
+    'voleibol',
+    'Qual o tipo de uniforme dos jogadores?',
+    'os jogadores devem usar um uniforme que possua uma cor dominante diferente de qualquer outra cor do uniforme do resto da equipe.'
+)
+
+criaCartao(
+    'boxe',
+    'Quais são os tipos de boxe?',
+    'Como kickboxing ou boxe estadunidense, muay thai ou boxe tailandês, boxe chinês ou sanda, boxe francês ou savate.'
+)
+
+criaCartao(
+    'video game',
+    'Qual foi o primeiro jogo eletrônico?',
+    'Higinbotham criou um dos primeiros videogames, Tennis for Two (“tênis para dois”), em 1958. '
+)
